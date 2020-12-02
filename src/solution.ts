b489a558b9a1111ab389f43ddc4dcbbe5a6ce7a2
Ian Harris <ianwebsterharris@gmail.com>
2020-12-02 13:06:21 -0700

    Clean up linter warnings

--- src/solution.ts
+++ src/solution.ts
@@ -7,10 +7,12 @@ import { getName } from './util/helper';
 export class BaseSolution implements Solution {
     public readonly description: string = '';
 
+    // eslint-disable-next-line no-unused-vars,@typescript-eslint/no-unused-vars
     solvePart1(lines: Input): string {
         throw new NotImplementedError(`solvePart1() is not implemented`);
     }
 
+    // eslint-disable-next-line no-unused-vars,@typescript-eslint/no-unused-vars
     solvePart2(lines: Input): string {
         throw new NotImplementedError(`solvePart2() is not implemented`);
     }
