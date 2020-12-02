b015ccad42c8f76a0269442ddf60eee2025f0541
Ian Harris <ianwebsterharris@gmail.com>
2020-12-01 23:20:13 -0700

    Fix test helper

--- test/test-helper.ts
+++ test/test-helper.ts
@@ -3,5 +3,5 @@ export { getName } from '../src/util/helper';
 import { Solution } from '../src/types';
 
 export const testName = (solution: Solution): string => {
-    return `${solution.id}: ${solution.name}`;
+    return solution.name;
 };
