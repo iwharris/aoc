af0416d87bc9a261fed67448e47f9d6e4c2d339b
Ian Harris <ianwebsterharris@gmail.com>
2020-12-10 16:34:27 -0700

    2020.10 P1

---
+++ test/challenge/2020.10.test.ts
@@ -0,0 +1,71 @@
+import { Solution } from '../../src/challenge/2020.10';
+import { testName } from '../test-helper';
+
+const solution = new Solution();
+
+describe(testName(solution), () => {
+    const input1 = solution.parseInput(
+        `
+        16
+        10
+        15
+        5
+        1
+        11
+        7
+        19
+        6
+        12
+        4
+        `
+    );
+
+    const input2 = solution.parseInput(
+        `
+        28
+        33
+        18
+        42
+        31
+        14
+        46
+        20
+        48
+        47
+        24
+        23
+        49
+        45
+        19
+        38
+        39
+        11
+        1
+        32
+        25
+        35
+        8
+        17
+        7
+        9
+        4
+        2
+        34
+        10
+        3
+        `
+    );
+
+    describe('Part 1', () => {
+        it('should solve part 1', () => {
+            expect(solution.solvePart1(input1)).toBe('35');
+            expect(solution.solvePart1(input2)).toBe('220');
+        });
+    });
+
+    describe('Part 2', () => {
+        it('should solve part 2', () => {
+            // expect(solution.solvePart2(input)).toBe('62');
+        });
+    });
+});
