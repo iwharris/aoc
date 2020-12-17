8f8d92a7d97d6ef36b386f0ffc0aaa958f18fb53
Ian Harris <ianwebsterharris@gmail.com>
2020-12-16 20:40:49 -0700

    2020.15 progress

---
+++ test/challenge/2020.15.test.ts
@@ -0,0 +1,20 @@
+import { Solution } from '../../src/challenge/2020.15';
+import { testName } from '../test-helper';
+
+const solution = new Solution();
+
+describe(testName(solution), () => {
+    const input = solution.parseInput(`0,3,6`);
+
+    describe('Part 1', () => {
+        it('should solve part 1', () => {
+            expect(solution.solvePart1(input)).toBe('436');
+        });
+    });
+
+    describe('Part 2', () => {
+        it('should solve part 2', () => {
+            //
+        });
+    });
+});
