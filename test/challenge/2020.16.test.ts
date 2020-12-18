95f33e1946a41d7538e16f8e320b5c14258d9505
Ian Harris <ianwebsterharris@gmail.com>
2020-12-17 21:20:05 -0700

    2020.16 P1

---
+++ test/challenge/2020.16.test.ts
@@ -0,0 +1,28 @@
+import { Solution } from '../../src/challenge/2020.16';
+import { parseInput } from '../../src/util/parser';
+import { testName } from '../test-helper';
+
+const solution = new Solution();
+
+describe(testName(solution), () => {
+    const input = solution.parseInput(
+        `class: 1-3 or 5-7
+        row: 6-11 or 33-44
+        seat: 13-40 or 45-50
+
+        your ticket:
+        7,1,14
+
+        nearby tickets:
+        7,3,47
+        40,4,50
+        55,2,20
+        38,6,12`
+    );
+
+    describe('Part 1', () => {
+        it('should solve part 1', () => {
+            expect(solution.solvePart1(input)).toBe('71');
+        });
+    });
+});
