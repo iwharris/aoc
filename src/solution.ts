60f9e96062b2aa85174dfa4ed5753ec5d2861090
Ian Harris <git@ianharris.io>
2022-12-02 23:30:29 -0700

    Expose class properties that override the default parsing behavior

--- src/solution.ts
+++ src/solution.ts
@@ -7,12 +7,21 @@ import { parseInput } from './util/parser';
 export abstract class BaseSolution implements Solution {
     public readonly description: string = '';
 
+    /** Set to true to preserve empty lines when parsing input. */
+    public readonly preserveEmptyLines: boolean = false;
+
+    /** Set to true to preserve leading/trailing whitespace when parsing input. */
+    public readonly preserveWhitespace: boolean = false;
+
     public abstract solvePart1(lines: Input): string;
 
     public abstract solvePart2(lines: Input): string;
 
     public parseInput(raw: string): Input {
-        return parseInput(raw);
+        return parseInput(raw, {
+            preserveEmptyLines: this.preserveEmptyLines,
+            preserveWhitespace: this.preserveWhitespace,
+        });
     }
 
     get name(): string {
