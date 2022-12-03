2b209b72abaf68dba6e4a82160da3486a51060ac
Ian Harris <git@ianharris.io>
2022-12-03 13:19:54 -0700

    Pull latest eslint config, update rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,11 @@
 module.exports = {
-    extends: [
-        '@iwharris/eslint-config',
-        'eslint:recommended',
-        'plugin:prettier/recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-    ],
+    extends: ['@iwharris/eslint-config'],
 
     rules: {
         'no-console': ['error', { allow: ['warn', 'error'] }],
         'no-fallthrough': ['warn'],
         'no-unsafe-finally': ['warn'],
+        'no-unused-vars': 0, // disable this because it's incorrectly flagging Typescript function literals
         'no-useless-escape': ['warn'],
         '@typescript-eslint/no-inferrable-types': ['warn'],
     },
