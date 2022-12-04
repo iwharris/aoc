bcba83d03d3e7ab5fa37d99dc16033657b1132dc
Ian Harris <git@ianharris.io>
2022-12-03 18:51:36 -0700

    2022.3 P2

--- src/challenge/2022.3.ts
+++ src/challenge/2022.3.ts
@@ -34,6 +34,31 @@ export class Solution extends BaseSolution {
     In the above example, the priority of the item type that appears in both compartments of each rucksack is 16 (p), 38 (L), 42 (P), 22 (v), 20 (t), and 19 (s); the sum of these is 157.
 
     Find the item type that appears in both compartments of each rucksack. What is the sum of the priorities of those item types?
+
+    --- Part Two ---
+    As you finish identifying the misplaced items, the Elves come to you with another issue.
+
+    For safety, the Elves are divided into groups of three. Every Elf carries a badge that identifies their group. For efficiency, within each group of three Elves, the badge is the only item type carried by all three Elves. That is, if a group's badge is item type B, then all three Elves will have item type B somewhere in their rucksack, and at most two of the Elves will be carrying any other item type.
+
+    The problem is that someone forgot to put this year's updated authenticity sticker on the badges. All of the badges need to be pulled out of the rucksacks so the new authenticity stickers can be attached.
+
+    Additionally, nobody wrote down which item type corresponds to each group's badges. The only way to tell which item type is the right one is by finding the one item type that is common between all three Elves in each group.
+
+    Every set of three lines in your list corresponds to a single group, but each group can have a different badge item type. So, in the above example, the first group's rucksacks are the first three lines:
+
+    vJrwpWtwJgWrhcsFMMfFFhFp
+    jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+    PmmdzqPrVvPwwTWBwg
+    And the second group's rucksacks are the next three lines:
+
+    wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+    ttgJtRGJQctTZtZT
+    CrZsJsPPZsGzwwsLwLmpwMDw
+    In the first group, the only item type that appears in all three rucksacks is lowercase r; this must be their badges. In the second group, their badge item type must be Z.
+
+    Priorities for these items must still be found to organize the sticker attachment efforts: here, they are 18 (r) for the first group and 52 (Z) for the second group. The sum of these is 70.
+
+    Find the item type that corresponds to the badges of each three-Elf group. What is the sum of the priorities of those item types?
     `;
 
     public solvePart1(lines: Input): string {
@@ -51,9 +76,19 @@ export class Solution extends BaseSolution {
         ).toString();
     }
 
-    // public solvePart2(lines: Input): string {
-    //     throw new NotImplementedError();
-    // }
+    public solvePart2(lines: Input): string {
+        return sum(
+            parseTrios(lines).map((trio) => {
+                const itemArrays = trio.map(parseBagToItemArray);
+
+                return sum(
+                    itemArrays[0].map((_, i) => {
+                        return itemArrays.every((arr) => arr[i] > 0) ? priorityOfIndex(i) : 0;
+                    })
+                );
+            })
+        ).toString();
+    }
 }
 
 const parseRucksacks = (line: string): [string, string] => {
@@ -64,6 +99,15 @@ const parseRucksacks = (line: string): [string, string] => {
     const midpoint = line.length / 2;
 
     return [line.slice(0, midpoint), line.slice(midpoint)];
+};
+
+type RucksackTrio = [string, string, string];
+const parseTrios = (lines: Input): Array<RucksackTrio> => {
+    const trios: Array<RucksackTrio> = [];
+    for (let i = 0; i < lines.length; i += 3) {
+        trios.push(lines.slice(i, i + 3) as RucksackTrio);
+    }
+    return trios;
 };
 
 const parseBagToItemArray = (bag: string): ItemArray => {
