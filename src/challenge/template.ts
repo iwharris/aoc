7a267d7dbaaa28199419b384cd55b2a59c0f8a20
Ian Harris <git@ianharris.io>
2023-12-26 11:27:11 -0700

    Add template.ts

---
+++ src/challenge/template.ts
@@ -0,0 +1,14 @@
+import { BaseSolution } from '../solution';
+import { Input } from '../types';
+
+export class Solution extends BaseSolution {
+    description = ``;
+
+    public solvePart1(lines: Input): string {
+        return '';
+    }
+
+    public solvePart2(lines: Input): string {
+        return '';
+    }
+}
